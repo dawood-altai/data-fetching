4d942831f7b425cec9f5d7089c951ff1a35dd6b4
dawood <dawood.altai111@gmail.com>
2023-05-18 22:19:16 +0300

    data fetch

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-# data-fetching
-
+/* 
 1- Create a program that fetches 10 users from the jsonplaceholderAPI
 
 2- Using the data you just fetched, create 10 user cards and show them on the screen.
@@ -11,3 +10,22 @@ PS: You definitely dont need to use all the data coming from the API, design and
 BONUS: Do a POST request to the https://jsonplaceholder.typicode.com/posts endpoint adding a new post there, and console log the result.
 
 
+*/
+
+async function fetchData() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await response.json();
+    console.log(data);
+
+    
+  
+  }
+
+  // const usersData = [];
+  // for (let i = 0; i < data.length; i++) {
+  //   usersData.push(data[i]);
+  //   console.log(data[i].name);
+  // }
+ fetchData();
+
+  
